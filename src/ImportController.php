643fa30b9b86d6aecc57884e7080d7b3c6488948
Sudip Dahal <sudip@dolphinwebsolutions.com>
2021-11-25 10:53:28 +0530

    added support to csv file as well

--- src/ImportController.php
+++ src/ImportController.php
@@ -22,23 +22,25 @@ class ImportController extends Controller
         $tables = DB::select('SHOW TABLES');
         $tables = array_map('current', $tables);
         $this->validate($request, [
-            'file' => 'required',
+            'file' => 'required|mimes:xlsx,csv,txt',
         ]);
         if ($request->hasFile('file')) {
             $file = $request->file('file');
-            // Generate a file name with extension
+            $originalFileName = $file->getClientOriginalName();
             $fileName = time() . '_excel_file_temp.' . $file->getClientOriginalExtension();
-            // Save the file
+            $fileExtension = $file->getClientOriginalExtension();
             $file->storeAs('public/files/excel', $fileName);
-
-            $collection = FastExcel::import($file)->toArray();
+            if ($fileExtension == "excel")
+                $collection = FastExcel::import($file)->toArray();
+            else
+                $collection = FastExcel::configureCsv()->import(public_path('storage/files/excel/' . $fileName))->toArray();
             $rows = array();
             foreach ($collection[0] as $row => $key) {
                 $rows[] = $row;
             }
             $rowInFile = $rows;
             Session::flash('success_message', 'Please select the row accordingly !');
-            return view('importify::process')->with(compact('rowInFile', 'action', 'tables', 'fileName'));
+            return view('importify::process')->with(compact('rowInFile', 'action', 'tables','fileName','fileExtension', 'originalFileName'));
         } else {
             Session::flash('error_message', 'Something went wrong!');
             return back();
@@ -57,7 +59,10 @@ class ImportController extends Controller
     public function process(Request $request)
     {
         $combinedData = array_combine($request->name, $request->value);
-        $collection = FastExcel::import(public_path('storage/files/excel/' . $request->fileName))->toArray();
+        if ($request->fileExtension == "excel")
+            $collection = FastExcel::import(public_path('storage/files/excel/' . $request->fileName))->toArray();
+        else
+            $collection = FastExcel::configureCsv()->import(public_path('storage/files/excel/' . $request->fileName))->toArray();
         foreach ($collection as $fileData => $fileDataKey) {
             $tmpData = [];
             foreach ($combinedData as $index => $key) {
@@ -66,16 +71,36 @@ class ImportController extends Controller
                 else
                     $tmpData[$index] = $fileDataKey[$key];
             }
-            $row = DB::table($request->table)->insert($tmpData);
+            $insertData[] = $tmpData;
         }
-        if ($row) {
+        for ($i = 0; $i <= count($insertData); $i++) {
+            if ($i == count($insertData))
+                break;
+            try {
+                test:
+                $row = DB::table($request->table)->insert($insertData[$i]);
+                $collection[$i]['Status'] = '1';
+                $collection[$i]['Remarks'] = 'Row Inserted Successfully';
+                $newData[] = $collection[$i];
+                $flag = 1;
+            } catch (\Exception $e) {
+                $collection[$i]['Status'] = '0';
+                $collection[$i]['Remarks'] = 'Row Insertion Failed';
+                $newData[] = $collection[$i];
+                $i++;
+                goto test;
+            }
+        }
+        $newData = collect($newData);
+        FastExcel::data($collection)->export('exported/'.$request->originalFileName);
+        $file = asset('exported/'.$request->originalFileName);
+        if ($flag) {
             unlink('storage/files/excel/' . $request->fileName);
             Session::flash('success_message', 'File inserted successfully.');
+            Session::flash('download.in.the.next.request', $file);
         } else {
             Session::flash('error_message', 'File insertion failed.');
         }
         return view('importify::add');
     }
-
 }
-
